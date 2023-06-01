0de3dbacb68272414b286874587c2252c7fd641c
purplecarrot625 <97133994+purplecarrot625@users.noreply.github.com>
2023-05-31 17:38:47 -0700

    Added submission features

--- models/submission.js
+++ models/submission.js
@@ -1,5 +1,9 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const { ObjectId, GridFSBucket } = require('mongodb');
+const fs = require('node:fs');
+
+const { getDbReference } = require('../lib/mongo');
 
 const SubmissionSchema = new Schema({
   assignmentId: {
@@ -26,3 +30,56 @@ const SubmissionSchema = new Schema({
 });
 
 module.exports = mongoose.model('Submission', SubmissionSchema);
+
+exports.saveFileInfo = async (file) => {
+  const db = getDbReference();
+  const collection = db.collection('files')
+  const result = await collection.insertOne(file)
+  return result.insertedId
+}
+
+exports.saveFile = async (file) => {
+  return new Promise((resolve, reject) => {
+    const db = getDbReference()
+    const bucket = new GridFSBucket(db, { bucketName: 'files' })
+    const metadata = {
+      contentType: file.contentType,
+      assignmentId: file.assignmentId,
+      studentId: file.studentId,
+      timestamp: file.timestamp,
+      grade: file.grade
+    }
+
+    const uploadStream = bucket.openUploadStream(
+      file.filename,
+      { metadata: metadata }
+    )
+    fs.createReadStream(file.path).pipe(uploadStream)
+      .on('error', (err) => {
+        reject(err)
+      }
+      ).on('finish', (result) => {
+        console.log("== write successful, result:", result)
+        resolve(result._id)
+      })
+    })
+}
+
+exports.getFileById = async (id) => {
+  const db = getDbReference()
+  const bucket = new GridFSBucket(db, { bucketName: 'files' })
+  if (!ObjectId.isValid(id)) {
+    return null
+  } else {
+    const results = await bucket.find({ _id: new ObjectId(id) })
+      .toArray()
+      console.log("== results:", results)
+      return results[0]
+  }
+}
+
+exports.getFileDownloadStreamByFilename = (filename) => {
+  const db = getDbReference()
+  const bucket = new GridFSBucket(db, { bucketName: 'files' })
+  return bucket.openDownloadStreamByName(filename)
+}
